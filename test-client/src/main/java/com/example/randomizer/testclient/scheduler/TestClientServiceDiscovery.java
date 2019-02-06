12b113b216c564b5c17d8728084eadd0649945bd
Developer <developer@me.com>
2019-02-06 20:47:53 +0100

    support for auto service discovery

---
+++ test-client/src/main/java/com/example/randomizer/testclient/scheduler/TestClientServiceDiscovery.java
@@ -0,0 +1,11 @@
+package com.example.randomizer.testclient.scheduler;
+
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.Status;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+
+public class TestClientServiceDiscovery {
+
+}
